7f10e028a499ca43f03616249fb784219876ed58
DARKYYYYYY <87261059+DARKYYYYYY@users.noreply.github.com>
2021-07-27 14:23:21 +0530

    Create utils.hh

---
+++ utils.hh
@@ -0,0 +1,18 @@
+
+#ifndef UTILS_HH
+#define UTILS_HH
+
+
+extern int min(int,int);
+
+extern int max(int,int);
+
+extern void swap(int *,int *);
+
+extern int pow(int,int);
+
+extern int rankAux(int,int,int *,int *);
+
+extern void unrankAux(int,int,int,int *);
+
+#endif
